39b282ac8d92af474422a44aa182923b23f7d1d6
babajsus <102524051+babajsus@users.noreply.github.com>
2022-10-09 22:00:37 +0300

    Create shesh.py

---
+++ shesh.py
@@ -0,0 +1,14 @@
+thisdict = {
+    "firma" : "ford",
+    "modelis" : "focus",
+    "izlaiduma gads" : "2022.gads",
+    "krasa" : "zila",
+
+}
+
+thisdict ["atrumkarba"] = "automats"
+thisdict.pop("izlaiduma gads")
+thisdict["krasa"] = "sarkans"
+
+print(thisdict)
+
